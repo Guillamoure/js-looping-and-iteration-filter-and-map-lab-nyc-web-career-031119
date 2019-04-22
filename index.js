29b3364c3923eb6a1023f57a1625fb83e6a33134
Jack Hilscher <Jack@Jacks-MacBook-Air.local>
2019-04-22 18:05:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here:
+function driversWithRevenueOver(array, num) {
+  return array.filter(driver => driver.revenue > num)
+}
+
+function driverNamesWithRevenueOver(array, num) {
+  objs = array.filter(driver => driver.revenue > num);
+  return objs.map(driver => driver.name)
+}
+
+function exactMatch(array, match){
+  let key = Object.keys(match)
+  return array.filter(driver => driver[key[0]] === match[key[0]])
+}
+
+function exactMatchToList(array, match) {
+  let key = Object.keys(match)
+  objs = array.filter(driver => driver[key[0]] === match[key[0]])
+  return objs.map(driver => driver.name)
+}
